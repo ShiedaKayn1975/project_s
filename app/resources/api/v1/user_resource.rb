2b7c95dad36d746767c6b025c20d9cb9a361f6b7
ShiedaKayn1975 <allmid66@gmail.com>
2022-06-12 23:00:30 +0700

    update

--- app/resources/api/v1/user_resource.rb
+++ app/resources/api/v1/user_resource.rb
@@ -1,3 +1,3 @@
 class Api::V1::UserResource < Api::V1::BaseResource
-    attributes :id, :name, :phone, :status, :admin, :created_at, :updated_at
+    attributes :id, :name, :phone, :status, :admin, :created_at, :updated_at, :balance
 end
